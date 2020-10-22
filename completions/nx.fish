8edb6cde0c62f6441dc3a5a65110a690e3f7bd1b
Jakub Benes <jukben@gmail.com>
2020-10-22 20:02:57 +0200

    support autocomplete for workspace-schematics as well (close #1)

--- completions/nx.fish
+++ completions/nx.fish
@@ -4,6 +4,13 @@ function __nx_run
     end
 end
 
+function __nx_workspace_schematic
+    if test -e tools/schematics/migrations.json; and type -q jq
+        jq -r '.schematics | keys | .[]' tools/schematics/migrations.json
+    end
+end
+
+
 set -l nx_commands run generate affected run-many affected:apps affected:libs affected:build affected:test affected:e2e affected:dep-graph print-affected affected:lint dep-graph format:check format:write workspace-lint workspace-schematic migrate report list
 
 complete -f -c nx -n "not __fish_seen_subcommand_from $nx_commands" -a generate -d 'Generate code'
@@ -20,8 +27,7 @@ complete -f -c nx -n "not __fish_seen_subcommand_from $nx_commands" -a affected:
 complete -f -c nx -n "not __fish_seen_subcommand_from $nx_commands" -a dep-graph -d 'Graph dependencies within workspace'
 complete -f -c nx -n "not __fish_seen_subcommand_from $nx_commands" -a format:check -d 'Check for un-formatted files'
 complete -f -c nx -n "not __fish_seen_subcommand_from $nx_commands" -a format:write -d 'Overwrite un-formatted files'
-complete -c nx -n "not __fish_seen_subcommand_from $nx_commands" -a workspace-lint -d 'Lint workspace or list of files'
-complete -f -c nx -n "not __fish_seen_subcommand_from $nx_commands" -a workspace-schematic -d 'Runs a workspace schematic from the tools/schematics directory'
+complete -f -c nx -n "not __fish_seen_subcommand_from $nx_commands" -a workspace-lint -d 'Lint workspace or list of files'
 complete -f -c nx -n "not __fish_seen_subcommand_from $nx_commands" -a migrate -d 'Creates a migrations file or runs migrations from the migrations file.'
 complete -f -c nx -n "not __fish_seen_subcommand_from $nx_commands" -a report -d 'Reports useful version numbers to copy into the Nx issue template'
 complete -f -c nx -n "not __fish_seen_subcommand_from $nx_commands" -a list -d 'Lists installed plugins, capabilities of installed plugins and other available plugins.'
@@ -30,3 +36,8 @@ complete -f -c nx -n "not __fish_seen_subcommand_from $nx_commands" -a list -d '
 complete -f -c nx -n "not __fish_seen_subcommand_from $nx_commands" -a run -d 'Run a target for a project'
 complete -f -c nx -n "__fish_seen_subcommand_from run; and not __fish_seen_subcommand_from (__nx_run)" -a "(__nx_run)"
 complete -f -c nx -n "__fish_seen_subcommand_from (__nx_run); and not __fish_seen_subcommand_from --" -a "--"
+
+# workspace-schematic
+complete -f -c nx -n "not __fish_seen_subcommand_from $nx_commands" -a workspace-schematic -d 'Runs a workspace schematic from the tools/schematics directory'
+complete -f -c nx -n "__fish_seen_subcommand_from workspace-schematic; and not __fish_seen_subcommand_from (__nx_workspace_schematic)" -a "(__nx_workspace_schematic)"
+complete -f -c nx -n "__fish_seen_subcommand_from (__nx_workspace_schematic); and not __fish_seen_subcommand_from --" -a "--"
